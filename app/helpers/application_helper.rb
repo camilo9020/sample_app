39a75e50d51971cd3372111497f7447cbe750dec
Juan Camilo <camilo9020@gmail.com>
2016-07-26 13:07:03 -0500

    Changing application title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,12 @@ module ApplicationHelper
 
  # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Twitter-Clone"
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
   end
-
-
-  
-
-
 
 end
